df67f45783d626d4fa98ea984cfcd4bbf10e66e1
CH <1925480119@qq.com>
2020-09-15 18:29:50 +0800

    [ChenHui]Return90WhenCalculateStudentAverageGradeAndGradeIs80And90And1009

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,6 +1,8 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GradeServiceTest {
@@ -8,8 +10,17 @@ public class GradeServiceTest {
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
 
+   GradeService gradeService;
+   StubGradeSystem stubGradeSystem;
+    @BeforeEach
+   public void setup(){
+        stubGradeSystem = new StubGradeSystem();
+        gradeService = new GradeService(stubGradeSystem);
+
+   }
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        double result = (long) gradeService.calculateAverageGrades(1L);
+        Assertions.assertEquals(90.0, result);
     }
 }
